daa7387a9768fef4d72cb113fe9de4f700de8c8e
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-06-03 16:00:12 -0400

    running asyn and await

--- server.js
+++ server.js
@@ -1,72 +1,88 @@
-const express = require('express')
-const pokemonData = require('./models/pokemon')
-//Set up dotenv 
-require('dotenv').config()
-// Using Node.js `require()`
-const mongoose = require('mongoose');
-// add a img property to the object
-const pokemonModel = require('./models/pokemonModel')
-
+const express = require("express");
+const pokemonData = require("./Models/pokemon");
+require("dotenv").config(); // configuration for dotenv
+const mongoose = require("mongoose");
+const PokemonModel = require("./Models/PokemonModel");
 
 //* ========== SETUP
-const app = express()
-const PORT = 3000
-app.set('view engine', 'ejs')
-app.set('views', './views')
+const app = express();
+const PORT = 3000;
+app.set("view engine", "ejs");
+app.set("views", "./Views");
 
 //* ======== Middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //* ========== ROUTES
-app.get('/', (req, res) => {
-    res.send('Welcome to the Pokemon App!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the Pokemon App!");
+});
 
-app.get('/pokemon', (req, res) => {
-    res.render('index', {
-        pageTitle: 'Pokemon',
-        pageHeader: 'See All The Pokemon!',
-        pokemonData: pokemonData
-    })
-})
+app.get("/pokemon", async (req, res) => {
+  try {
+    // fetch data from the db
+    const pokemons = await PokemonModel.find();
 
-app.get('/pokemon/new', (req, res) => {
-    res.render('new', {
-        pageTitle: 'New Pokemon',
-        pageHeader: 'Create a new Pokemon'
-    })
-})
+    res.render("Index", {
+      pageTitle: "Pokemon",
+      pageHeader: "See All The Pokemon!",
+      pokemonData: pokemons,
+    });
+  } catch (error) {
+      console.log(error);
+  }
+});
+
+app.get("/pokemon/new", (req, res) => {
+  res.render("New", {
+    pageTitle: "New Pokemon",
+    pageHeader: "Create a new Pokemon",
+  });
+});
 
 //* POST REQUEST HANDLER
-app.post('/pokemon', async (req, res) => {
-    const newPokemon = req.body //create a new pokemon variable and save object in it
-    //add an img property to the object
-    newPokemon.img = `http://img.pokemondb.net/artwork/${req.body.name}`
-    console.log(newPokemon)
-    // Save the new pokemon to the db
-    await pokemonModel.create(newPokemon, (error, result) => {
-        if (error) {
-            console.log(error)
-        }
-        console.log(result)
-    })
-})
+app.post("/pokemon", async (req, res) => {
+  const newPokemon = req.body; // create a newPokemon variable
+  // add a img property to the object
+  newPokemon.img = `http://img.pokemondb.net/artwork/${req.body.name.toLowerCase()}`;
 
-app.get('/pokemon/:id', (req, res) => {
-    // res.send(req.params.id)
+  console.log(newPokemon);
 
-    res.render('show', {
-        pageTitle: 'Details',
+  //* Save the new pokemon to the db
+  await PokemonModel.create(newPokemon, (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+res.redirect('/pokemon')
+    console.log(result);
+  });
+
+  // More stuff
+});
+
+app.get("/pokemon/:id", async (req, res) => {
+  try {
+      console.log(req.params.id)
+      const pokemon = await PokemonModel.findById(req.params.id)
+      console.log('POKEMON FOUND!', pokemon);
+
+    res.render("Show", {
+        pageTitle: "Details",
         pageHeader: " Gotta Catch 'Em All ",
-        pokemon: pokemonData[req.params.id]
-    })
-})
+        pokemon: pokemon,
+      });
+
+  } catch (error) {
+      console.log(error)
+  }
+
+  
+});
 
 //* =========== LISTENER
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-    mongoose.connect(process.env.MONGODB_URI)// We use Mongoose to connect to mongoDB 
-    console.log('MongoDB Connected')
-})
+  console.log(`Server is running on port: ${PORT}`);
+  mongoose.connect(process.env.MONGODB_URI); // CONNECTS TO MONGO DB
+  console.log("MongoDB connected!");
+});
